fd2d92d12090abebbfd2b980a4e9ec80ea33bfbc
Alexandr <oleksandrglushen2005@gmail.com>
2025-07-28 18:11:03 +0300

    test code

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import models
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+import taxi
 from taxi.models import Driver, Car, Manufacturer
 
 
@@ -13,3 +16,37 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer").all()
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.all()
+
+
+def driver_detail_view(request, pk):
+    driver = Driver.objects.get(pk=pk)
+    cars = driver.cars.select_related("manufacturer").all()
+
+    return render(request, "taxi/driver_detail.html",
+                  {"driver": driver, "cars": cars})
